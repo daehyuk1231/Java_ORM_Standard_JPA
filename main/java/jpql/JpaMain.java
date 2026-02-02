c0cc293287873ef0749788b524de727b423eb440
daehyuk1231 <daehyuk1231@naver.com>
2026-02-02 16:14:33 +0900

    jpql 타입 표현과 기타식

---
+++ main/java/jpql/JpaMain.java
@@ -0,0 +1,48 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m.username, 'HELLO', TRUE From Member m " +
+                            "where m.type = :userType";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .setParameter("userType", MemberType.ADMIN)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
