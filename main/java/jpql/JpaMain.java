b366987adb90016109fa26d4a865b18bf92e4966
daehyuk1231 <daehyuk1231@naver.com>
2026-02-02 16:29:58 +0900

    조건식(CASE 등등)

--- main/java/jpql/JpaMain.java
+++ main/java/jpql/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
@@ -30,10 +30,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', TRUE From Member m " +
-                            "where m.type = :userType";
-            List<Member> result = em.createQuery(query, Member.class)
-                    .setParameter("userType", MemberType.ADMIN)
+            String query = "select nullif(m.username, '관리자') as username " +
+                    " from Member m ";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
             tx.commit();
