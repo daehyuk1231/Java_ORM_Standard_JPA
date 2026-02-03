c81a9134df16507500153d033097238c480dd46f
daehyuk1231 <daehyuk1231@naver.com>
2026-02-03 15:51:24 +0900

    엔티티 직접 사용

--- main/java/jpql/JpaMain.java
+++ main/java/jpql/JpaMain.java
@@ -43,18 +43,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t join fetch t.members";
+            String query = "select m From Member m where m.team = :team";
 
-            List<Team> result = em.createQuery(query, Team.class)
+            List<Member> members = em.createQuery(query, Member.class)
+                    .setParameter("team", teamA)
                     .getResultList();
 
-            System.out.println("result = " + result.size());
-
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + "Members: " + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("-> member = " + member);
-                }
+            for (Member member : members) {
+                System.out.println("member = " + member);
             }
 
             tx.commit();
